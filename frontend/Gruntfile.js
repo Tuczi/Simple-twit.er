44627dd9228de8ea714fb11d4a7e89091fe7371d
Tuczi <tuczi007@gmail.com>
2014-07-19 20:04:24 +0000

    grunt added. project structured changed

---
+++ frontend/Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+	
+grunt.initConfig({
+	pkg: grunt.file.readJSON('package.json'),
+	concat: {
+		options: {
+			separator: ';'
+		},
+		dist: {
+			src: ['src/**/*.js'],
+			dest: 'build/<%= pkg.name %>.js'
+		}
+	},
+	uglify: {
+		options: {
+		banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+		},
+		dist: {
+			files: {
+				'build/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+			}
+		}
+	},
+	jshint: {
+		files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+		options: {
+			// options here to override JSHint defaults
+			globals: {
+				jQuery: true,
+				console: true,
+				module: true,
+				document: true
+			}
+		}
+	}
+});
+
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-sass');
+grunt.loadNpmTasks('grunt-contrib-concat');
+
+grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+
+};
