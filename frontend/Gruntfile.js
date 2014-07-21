12863e077ede47701a834614a7e320dc71188d2d
Tuczi <tuczi007@gmail.com>
2014-07-21 18:28:02 +0000

    grunt-include-source added. new grunt tasks (dev, prod) added. TODO fixed dev task - missing included js and css files

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -23,7 +23,7 @@ grunt.initConfig({
 		}
 	},
 	jshint: {
-		files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+		files: ['Gruntfile.js', 'src/assets/javascripts/**/*.js', 'test/assets/javascripts/**/*.js'],
 		options: {
 			globals: {
 				jQuery: true,
@@ -34,21 +34,55 @@ grunt.initConfig({
 		}
 	},
 	sass: {
-		dist: {
+		dev: {
+			files: [{
+				expand: true,
+				cwd: 'src/assets/stylesheets',
+				src: ['*.css.scss'],
+				dest: 'build/assets/stylesheets',
+				ext: '.css'
+			}]
+		},
+		prod: {	
 		    options: {
 				style: 'nested'
 			},
-			files: {
-				'build/assets/stylesheets/style.css': 'src/assets/stylesheets/main.css.scss'
-			}
+			files: { 'build/assets/stylesheets/main.css': 'src/assets/stylesheets/main.css.scss' }
 		}
 	},
 	copy: {
-		main: {
+		dev: {
 			files: [
-				{ expand: true, cwd: 'src', src: '**', dest: 'build/' },
-				{ src: 'src/index.html', dest: 'build/index.html' } 
+				{ expand: true, cwd: 'src/views', src: '**', dest: 'build/views' },
+				{ src: 'src/index.html', dest: 'build/index.html' },
+				{ expand: true, cwd: 'src/assets/javascripts', src: '**', dest: 'build/assets/javasctips'}
 			]
+		},
+		prod: {
+			files: [
+				{ expand: true, cwd: 'src/views', src: '**', dest: 'build/views' },
+				{ src: 'src/index.html', dest: 'build/index.html'}
+			]				
+		},
+		apache: {
+			files: [{ expand: true, cwd: 'build', src: '**', dest: '/srv/http/angular' }]
+		}
+	},
+	includeSource: {
+		options: {
+			templates: {
+				html: {
+					js: '<script src="{filePath}"></script>',
+					css: '<link rel="stylesheet" type="text/css" href="{filePath}" />'
+				},
+				basePath: 'build/',
+				baseUrl: '/'
+			}
+		},
+		dev: {
+			files: {
+				'build/index.html': 'build/index.html'
+			}
 		}
 	}
 });
@@ -59,10 +93,12 @@ grunt.loadNpmTasks('grunt-contrib-sass');
 grunt.loadNpmTasks('grunt-contrib-concat');
 grunt.loadNpmTasks('grunt-contrib-clean');
 grunt.loadNpmTasks('grunt-contrib-copy');
+grunt.loadNpmTasks('grunt-include-source');
 
-grunt.registerTask('default', ['clean', 'jshint', 'sass', 'concat', 'uglify', 'copy']);
+grunt.registerTask('default', ['build:dev']);
 
-grunt.registerTask('build:dev', ['clean', 'jshint', 'sass', 'concat', 'uglify', 'copy']);//TODO do not concat and uglify
-grunt.registerTask('build:production', ['clean', 'jshint', 'sass', 'concat', 'uglify', 'copy']);
+grunt.registerTask('build:dev', ['clean', 'jshint', 'sass:dev', 'copy:dev', 'includeSource:dev']);
+grunt.registerTask('build:prod', ['clean', 'jshint', 'sass:prod', 'concat', 'uglify', 'copy:prod']);
+grunt.registerTask('build:apache', ['build:prod', 'copy:apache']);
 
 };
